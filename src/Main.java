4cd92df2c428ef482ef95d053df2ddf11795a5d5
Michael Lee Piotrowski <mlp95747@gmail.com>
2024-07-01 10:40:32 -0700

    V 2.0 Added a feature to print every 2 seconds

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-
+        System.out.println("Hello and welcome!");
+        int count = 0;
+        try{
+            while (true){
+                Thread.sleep(2 * 1000);
+                System.out.println("I'm still here! Iteration " + count++);
+            }
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
     }
 }
